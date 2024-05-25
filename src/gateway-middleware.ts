e2ef08cb73bc84f010600926c5105fc6322ba6ab
Edward Kuan <edward880814@gmail.com>
2024-05-25 23:32:26 +0800

    update library

--- src/gateway-middleware.ts
+++ src/gateway-middleware.ts
@@ -33,7 +33,10 @@ export function verifyGatewayRequest(
   }
 
   try {
-    const payload: { id: string; iat: number } = JWT.verify(token, '') as {
+    const payload: { id: string; iat: number } = JWT.verify(
+      token,
+      '1282722b942e08c8a6cb033aa6ce850e'
+    ) as {
       id: string;
       iat: number;
     };
